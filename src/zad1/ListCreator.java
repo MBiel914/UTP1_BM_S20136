a74a39b86c3e7483d3b93da06545e7096eba3363
Michał Bielecki <mbielecki@MacBook-Pro.local>
2021-03-13 11:55:50 +0100

    Org

---
+++ src/zad1/ListCreator.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Bielecki Michał S20136
+ *
+ */
+
+package zad1;
+
+
+public class ListCreator { // Uwaga: klasa musi być sparametrtyzowana
+}  
---
+++ src/zad1/Mapper.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Bielecki Michał S20136
+ *
+ */
+
+package zad1;
+
+
+public interface Mapper { // Uwaga: interfejs musi być sparametrtyzowany
+}  
