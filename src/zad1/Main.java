3c258605d04e8078cc4f0f4814a1d3dfbcf5b827
Michał Bielecki <mbielecki@MacBook-Pro.local>
2021-03-13 12:02:35 +0100

    Base zad1 wordking

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -12,27 +12,27 @@ import java.util.*;
 
 public class Main {
   public Main() {
-    List<Integer> src1 = /*<-- tu dopisać inicjację elementów listy */
+    List<Integer> src1 = Arrays.asList(1, 7, 9, 11, 12);/*<-- tu dopisać inicjację elementów listy */
     System.out.println(test1(src1));
 
-    List<String> src2 = /*<-- tu dopisać inicjację elementów listy */
+    List<String> src2 = Arrays.asList("a", "zzzz", "vvvvvvv" );/*<-- tu dopisać inicjację elementów listy */
     System.out.println(test2(src2));
   }
 
   public List<Integer> test1(List<Integer> src) {
-    Selector /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
-    Mapper   /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
-    return   /*<-- zwrot wyniku
+//    Selector /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
+//    Mapper   /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
+    return null;  /*<-- zwrot wyniku
       uzyskanego przez wywołanie statycznej metody klasy ListCreator:
-     */  collectFrom(src).when(sel).mapEvery(map);
+     */  //collectFrom(src).when(sel).mapEvery(map);
   }
 
   public List<Integer> test2(List<String> src) {
-    Selector /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
-    Mapper   /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
-    return   /*<-- zwrot wyniku
+//    Selector /*<-- definicja selektora; bez lambda-wyrażeń; nazwa zmiennej sel */
+//    Mapper   /*<-- definicja mappera; bez lambda-wyrażeń; nazwa zmiennej map */
+    return  null; /*<-- zwrot wyniku
       uzyskanego przez wywołanie statycznej metody klasy ListCreator:
-     */  collectFrom(src).when(sel).mapEvery(map);
+     */  //collectFrom(src).when(sel).mapEvery(map);
   }
 
   public static void main(String[] args) {
